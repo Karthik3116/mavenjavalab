5ec7f9bbf125740ba70d8105b3bc3e7a2f45a34a
karthik <150332751+Yaminiamrutham@users.noreply.github.com>
2024-12-30 23:20:25 +0530

    first commit

---
+++ src/test/java/com/lab/demo/maven_java_lab/AppTest.java
@@ -0,0 +1,20 @@
+package com.lab.demo.maven_java_lab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
